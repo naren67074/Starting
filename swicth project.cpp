013617b6a32c23402ad6254a491c808eb786d6c9
naren67074 <36063086+naren67074@users.noreply.github.com>
2018-03-18 15:29:24 +0530

    i want its correction pls
    i want to function with switch using

---
+++ swicth project.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+int calsum(int x, int y, int z);
+int main()
+{
+	int i,sum,a,b,c;
+	int num=1;
+	switch(i);
+	printf("Enter any three numbers");
+	scanf("%d%d%d",&a, &b, &c);
+	sum=calsum(a,b,c);
+	printf("Sum=%d\n",sum);
+	scanf("%d",&num);
+	return 0;
+}
+int calsum(int x, int y, int z)
+{	int d;
+	d=x+y+z;
+	return(d);
+}
+int i()
+	switch(i);
+{
+	case 1:
+	int i;
+	printf("Enter value of i");
+	scanf("%d",&i);
+	switch(i)
+	{
+		printf("Hello Friend\n");
+			printf("dsffhgh");
+			break;
+	}
+	return 0;
+}
